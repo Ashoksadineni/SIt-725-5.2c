8c230c5ed18dfae22b33e0b3fbc793629bc1e26d
Ashoksadineni <s225149067@deakin.edu.au>
2025-04-16 10:57:53 +1000

    6.2d

---
+++ Task6.2D/public/js/script.js
@@ -0,0 +1,68 @@
+
+const cardList = [
+    {
+        title: "fertilizer",
+        image: "images/fertilizer .jpeg",
+        link: "About fertilizer",
+        desciption: " The sprayer is loaded with liquid fertiliser to efficiently distribute the fertiliser to the crops"
+    },
+    {
+        title: "Harvester",
+        image: "images/Harvester.jpeg",
+        link: "About  Harvester",
+        desciption: "in the context of farming refers to a machine, often a combine harvester, that cuts, threshes, and cleans crops, streamlining the harvesting process and saving labor and time"
+    }
+]
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text card-desc-color">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+// const clickMe = () => {
+//     alert("Thanks for clicking me. Hope you have a nice day!")
+// }
+const getProjects = () => {
+    $.get('/api/projects',(response) => {
+        if(response.statusCode==200){
+            addCards(response.data);
+        }
+    })
+}
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+    $.get('/api/projects', formData, (response)=>{
+        if(response.statusCode==200){
+            // addCards(response.data);
+        }
+
+        
+        
+    });
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+    // getprojects();
+    addCards(cardList);
+    $('.modal').modal();
+}); 
